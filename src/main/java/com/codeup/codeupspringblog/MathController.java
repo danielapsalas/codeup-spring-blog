163ff35782b3486fb0ef0bc6d4b1b18c4e806a91
Daniela Salas <danielapiconsalas@gmail.com>
2023-05-17 11:58:37 -0500

    HomeController and MathController completed

---
+++ src/main/java/com/codeup/codeupspringblog/MathController.java
@@ -0,0 +1,34 @@
+package com.codeup.codeupspringblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MathController {
+
+    @GetMapping("/add/{num1}/and/{num2}")
+    @ResponseBody
+    public String addNums(@PathVariable int num1, @PathVariable int num2) {
+        return String.valueOf(num1 + num2);
+    }
+
+    @GetMapping("/subtract/{num1}/from/{num2}")
+    @ResponseBody
+    public String subtractNums(@PathVariable int num1, @PathVariable int num2) {
+        return String.valueOf(num1 - num2);
+    }
+
+    @GetMapping("/multiply/{num1}/and/{num2}")
+    @ResponseBody
+    public String multiplyNums(@PathVariable int num1, @PathVariable int num2) {
+        return String.valueOf(num1 * num2);
+    }
+
+    @GetMapping("/divide/{num1}/by/{num2}")
+    @ResponseBody
+    public String divideNums(@PathVariable int num1, @PathVariable int num2) {
+        return String.valueOf(num1 * num2);
+    }
+}

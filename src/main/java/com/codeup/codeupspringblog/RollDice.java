b154e2607a0922f3a9fbccf43ce377683af32173
Daniela Salas <danielapiconsalas@gmail.com>
2023-05-18 11:17:08 -0500

    completed dice roll and created three partials

---
+++ src/main/java/com/codeup/codeupspringblog/RollDice.java
@@ -0,0 +1,34 @@
+package com.codeup.codeupspringblog;
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Random;
+
+@Controller
+public class RollDice {
+//    @RequestParam int randomNum, Model model
+    @GetMapping("/rollDice")
+    public String rollDice (){
+        return "guessNum";
+    }
+
+    @GetMapping("/rollDice/{n}")
+    public String rollDiceNumber(@PathVariable int n, Model model) {
+        int randomNumber = (int)Math.floor(Math.random() * (6 - 1 + 1) + 1);
+        String guess = "";
+
+        if(randomNumber == n) {
+            guess = "Yay! You guessed correctly";
+        }else{
+            guess = "Nice try but you're incorrect...";
+        }
+
+        model.addAttribute("userGuess", "You guessed: " + n);
+        model.addAttribute("diceRoll", "dice rolled " + randomNumber);
+        model.addAttribute("guess", guess);
+
+        return "guessNum";
+    }
+
+}
